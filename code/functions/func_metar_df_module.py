a33905310d3c2d4abbd8d1f819feed45dfa7cf7e
Gabriel Sakovitz <gsakovitz@gmail.com>
2024-01-30 02:03:14 -0300

    files

--- code/functions/func_metar_df_module.py
+++ code/functions/func_metar_df_module.py
@@ -4,11 +4,11 @@ from io import StringIO
 
 def metar_processing():
     # puxa json como dataframe
-    df = pd.read_json('D:\Data Scientist\MBA\TCC\\repo\\related_archives\\response.json')
+    df = pd.read_json('D:\Data Scientist\MBA\TCC\\repo\weather-delay-investigation-MBA-\code\\functions\\related_archives\\response.json')
     # transforma e exporta o dataframe em csv 
-    df.to_csv('D:\Data Scientist\MBA\TCC\\repo\\related_archives\metar.csv', index=False)
+    df.to_csv('D:\Data Scientist\MBA\TCC\\repo\weather-delay-investigation-MBA-\code\\functions\\related_archives\metar.csv', index=False)
 
-    all_metars = '\n'.join(line.split(',')[2] for line in open('D:\Data Scientist\MBA\TCC\\repo\\related_archives\metar.csv', 'rt'))
+    all_metars = '\n'.join(line.split(',')[2] for line in open('D:\Data Scientist\MBA\TCC\\repo\weather-delay-investigation-MBA-\code\\functions\\related_archives\metar.csv', 'rt'))
     df = metpy.io.parse_metar_file(StringIO(all_metars))
 
     # documentação de colunas https://unidata.github.io/MetPy/latest/api/generated/metpy.io.parse_metar_file.html
@@ -18,5 +18,4 @@ def metar_processing():
 
     # deletar colunas
     df = df.drop(columns=colunas_para_deletar)
-
     return df
