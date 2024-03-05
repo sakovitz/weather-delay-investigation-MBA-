367684880beb529733e3186b9f3c2f3e5d77c959
Gabriel Sakovitz <gsakovitz@gmail.com>
2024-03-05 01:32:52 -0300

    exclusao de print na funcao func_metar_df_module

--- code/functions/func_metar_df_module.py
+++ code/functions/func_metar_df_module.py
@@ -25,7 +25,4 @@ def metar_processing(path, archive_name):
     # Subtrair 3 meses e 1 ano
     df['date_time'] = df['date_time'] - pd.DateOffset(months=3, years=1)
 
-    # Verifique se a transformação foi feita corretamente
-    print(df['date_time'].head())
-
     return df
