1ce6f1a10d018c1ec0f5f5c8d908a48a54184eb5
Gabriel Sakovitz <gsakovitz@gmail.com>
2024-03-07 04:54:24 -0300

    R analysis before bed2

--- r_analysis/departures_regression.R
+++ r_analysis/departures_regression.R
@@ -3,14 +3,40 @@ departure <- read.csv("departure_logs.csv",
                       sep = ",",
                       dec = ".")
 
+# Load departures log csv file
+arrival <- read.csv("arrival_logs.csv",
+                      sep = ",",
+                      dec = ".")
+
 # Check types via Glimpse
 glimpse(departure)
 
 # look some statistics
 summary(departure)
 
+
 # Selecionando apenas as colunas 3 e 21
-data_subset <- departure[, c(3, 7, 5, 6, 12, 19, 20, 21)]
+data_subset <- departure[, c(2, 7, 5, 6, 12, 19, 20, 21)]
+
 
 # Usando chart.Correlation no subset
 chart.Correlation(data_subset, histogram = TRUE)
+
+
+# Selecionando apenas as colunas 3 e 21
+data_subset_ <- arrival[, c(2, 4, 7, 5, 6, 8, 12, 19, 20, 21)]
+
+# need to transform Y column into dummy
+arrival_dummy <- dummy_columns(.data = data_subset_,
+                               select_columns = "arrival_status",
+                               remove_selected_columns = TRUE,
+                               remove_most_frequent_dummy = TRUE)
+
+# need to transform current_weather column into dummy
+arrival_dummy <- dummy_columns(.data = arrival_dummy,
+                               select_columns = "current_wx1",
+                               remove_selected_columns = TRUE,
+                               remove_most_frequent_dummy = TRUE)
+
+# Usando chart.Correlation no subset
+chart.Correlation(arrival_dummy, histogram = TRUE)
